2dbb84f24c25347029fe629329dda38dc5d06fe3
yadhu krishna <yadhukris>
2025-05-28 10:01:42 +0530

    yadhiuj43425i3777777777

--- src/components/homepage/home.jsx
+++ src/components/homepage/home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
   
         const { data } = await axios.post(
           "https://zerodhabackend-tszm.onrender.com/verify",
-          
+           {},
           { withCredentials: true }
         );
 
