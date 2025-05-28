4406c6e4ec6cc43e38c25fdd26f129bc91df482c
yadhu krishna <yadhukris>
2025-05-28 11:41:01 +0530

    yadhiuj43425i3777777777

--- src/components/homepage/signcontext.jsx
+++ src/components/homepage/signcontext.jsx
@@ -6,7 +6,7 @@ import { createContext, useState } from "react";
 export const Signcontext = createContext();
 
 export  function SignedContextprovider({children}){
-let[Signnn,setSignnn] = useState(false);
+let[Signnn,setSignnn] = useState(true);
 
     return(
         <Signcontext.Provider value={{Signnn,setSignnn}} >
