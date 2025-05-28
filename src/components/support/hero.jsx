df4b0dca3ccc14fc8bdf55025abefd015d7ccc36
yadhu krishna <yadhukris>
2025-05-28 10:08:49 +0530

    yadhiuj43425i3777777777

--- src/components/support/hero.jsx
+++ src/components/support/hero.jsx
@@ -4,7 +4,7 @@ export default function Hero(){
             <h4 style={{marginLeft:"2rem"}} className="mb-5">Support Portal</h4>
             <div className="row">
            
-                <div className="col-6 p-5">
+                <div id="support-div" className="co-md-6 p-5">
 
 <h3>Search for an answer or browse help topics to create a ticket</h3>
 <input style={{width:"100%",height:"3rem",margin:"1rem 0rem 1rem 0rem"}} type="text" placeholder="Eg: How do i activate account ?" />
@@ -18,7 +18,7 @@ export default function Hero(){
 
 
                 </div>
-                <div className="col-6 p-5">
+                <div  id="support-div" className="col-md-6 p-5">
 <h3>Featured</h3>
 <ul style={{listStyleType: "decimal"}}>
 <li> <a href="#">Current takeover and Dislisting -january 2025</a>  </li>
