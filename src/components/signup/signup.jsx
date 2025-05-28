43919ca45c854c11200d646871dcc5f39e5ba4fc
yadhu krishna <yadhukris>
2025-05-28 19:14:02 +0530

    yadhiuj43425i3777777777

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -2,7 +2,7 @@ import { useState ,useContext } from "react"
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
-
+import 'react-toastify/dist/ReactToastify.css';
 import { Signcontext } from "../homepage/signcontext";
 
 
